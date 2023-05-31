ef8b55dc226e07a4925a2a83686dc3fd47058f86
Avnish Kotambkar <avnishkotambkar@gmail.com>
2023-05-31 16:26:33 +0530

    Adding app python file

---
+++ app.py
@@ -0,0 +1,24 @@
+print("Hello World")
+age = 20
+price = 19.95
+first_name = "Avnish"
+is_online = False
+print(age)
+
+name = "John Smith"
+Age = "20 years old"
+Patient = "New"
+
+Name = input("What is your name? ")
+print("Hello " + Name)
+
+birth_year = int(input("Enter your birth year: "))
+AGE = 2023 - birth_year
+print(AGE)
+
+
+
+
+
+
+
